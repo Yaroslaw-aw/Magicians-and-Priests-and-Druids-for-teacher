1c65959399f78e51aafa9d37becd833ff5084f87
Yaroslaw-aw <manumission@mail.ru>
2023-08-26 23:09:42 +0300

    Comments corrections

--- Program.cs
+++ Program.cs
@@ -15,26 +15,32 @@ namespace Magicians_and_Priests_and_Druids
             hero4.GetInfo();
             hero1.GetInfo();
             Console.WriteLine(new string('-', 57));
-            ((Magician)hero2).Attack(hero1);
-            ((Magician)hero1).Attack(hero4);
-            ((Magician)hero1).Attack(hero4);
-            ((Magician)hero1).Attack(hero4);
+            ((Magician)hero2).Attack(hero1); // Маг2 атакует Мага1
+            ((Magician)hero1).Attack(hero4); // Маг1 атакует Друида4
+            ((Magician)hero1).Attack(hero4); // Маг1 атакует Друида4
+            ((Magician)hero1).Attack(hero4); // Маг1 атакует Друида4
 
             hero1.GetInfo();
             Console.WriteLine(new string('-', 57));
             hero4.GetInfo();
             Console.WriteLine(new string('-', 57));
-            ((IHealer)hero4).Heal(hero1);
-            ((IHealer)hero4).Heal(hero1);
-            ((IHealer)hero4).Heal(hero1);
+            ((IHealer)hero4).Heal(hero1); // Друид4 лечит Мага1
+            ((IHealer)hero4).Heal(hero1); // Друид4 лечит Мага1
+            ((IHealer)hero4).Heal(hero1); // Друид4 лечит Мага1
             hero4.GetInfo();
             Console.WriteLine(new string('-', 57));
             hero1.GetInfo();
             Console.WriteLine(new string('-', 57));
-            hero1.GetDamage(80);
+            hero1.GetDamage(80); // Маг1 бьет сам себя - это проблема из-за того, что метод получения урона в базовом классе - публичный.
+                                 // Если его сделать защищенным, тогда нельзя будет каким-то персонажем ударить другого персонажа.
+                                 // Точнее можно будет попытаться, но метод получения урона можно будет вызвать только на собственном экземпляре, т.е. ударить самого себя...
+                                 
             hero1.GetInfo();
             Console.WriteLine(new string('-', 57));
-            hero1.Healed(500);
+            hero1.Healed(500); // По аналогии Маг1 лечит сам себя из-за того, что опять же в базовом классе открытый метод получения лечения.
+                               // У мага вообще никакого лечения не должно быть, т.к. по нашей логике лечить могут только Друиды.
+                               // 
+                               // Так вот, как решить эту проблему?
             hero1.GetInfo();
         }
     }
